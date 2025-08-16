ca850d5d0c207214c2a1c8a090f44fcd94ec3ecd
Yoodeit <juno8147@gmail.com>
2025-08-16 16:43:08 +0900

    view changed

--- reDjango/accountapp/views.py
+++ reDjango/accountapp/views.py
@@ -2,11 +2,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import Helloworld
+
 # Create your views here.
 
 def helloworld(request):
     #return HttpResponse('hello')
     if request.method == "POST":
-        return render(request, 'accountapp/helloworld.html', context={'text': 'POST METHOD desu'})
+
+        temp = request.POST.get('helloworld_input')
+
+        new_helloworld = Helloworld();
+        new_helloworld.text = temp
+        new_helloworld.save()
+        
+        return render(request, 'accountapp/helloworld.html', context={'helloworld_output': new_helloworld})
     else:
         return render(request, 'accountapp/helloworld.html', context={'text': 'GET METHOD desu'})
