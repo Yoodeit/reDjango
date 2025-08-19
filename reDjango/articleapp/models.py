a4705cb416ff7fdd4d5bd6553411f4473c5366ad
Yoodeit <juno8147@gmail.com>
2025-08-19 23:10:30 +0900

    article part almost done

--- reDjango/articleapp/models.py
+++ reDjango/articleapp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    # 유저가 지워지면 (알수없음) 이런식으로 뜨도록 하는 세팅이 SET_NULL, related name은 유저객체에서 접근하기 쉬우라고
+    title = models.CharField(max_length=200, null=True)
+    # 제목은 캐릭터필드. 최대길이 200에 null 허용
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+    created_at = models.DateField(auto_created=True, null=True)
+
+    
+
